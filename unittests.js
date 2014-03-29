42b230ca71711759c222fddc9848902f4967b5e5
Razvan Caliman <razvan.caliman@gmail.com>
2014-03-29 12:20:31 +0000

    update tests to new shapes syntax

--- unittests.js
+++ unittests.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+ * Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -36,9 +36,21 @@ define(function (require, exports, module) {
         main                        = require("main");
 
     describe("CSS Shapes Editor", function(){
+        var testDocument, testEditor;
 
+        function constructModelAtPos (row, col) {
+            testEditor.setCursorPos(row, col);
+            // mutates main.model
+            main._constructModel({ target: testEditor });
+        }
+
+        /*
+          Quick test for shape-like values in Brackets.
+
+          Actual shape conformity is tested by the thirdparty CSSShapesEditor.js
+          It will throw errors for invalid shapes; to be caught by the Brackets extension.
+        */
         describe('Positive match CSS Shapes-like values', function(){
-            var testDocument, testEditor;
 
             beforeEach(function () {
                 var mock = SpecRunnerUtils.createMockEditor(testContentMatchPositive, "css");
@@ -60,95 +72,85 @@ define(function (require, exports, module) {
             })
 
             it("should match shape-inside property", function () {
-                testEditor.setCursorPos(3, 20);
+                constructModelAtPos(3, 20);
                 expect(main.model.get('property')).toBe('shape-inside');
             });
-            
+
             it("should not match property or value when cursor is after semicolon", function () {
-                testEditor.setCursorPos(3, 28);
+                constructModelAtPos(3, 28);
                 expect(main.model.get('property')).not.toBe('shape-inside');
                 expect(main.model.get('value')).not.toBe('circle()');
             });
 
             it("should match shape-outside property", function () {
-                testEditor.setCursorPos(4, 21);
+                constructModelAtPos(4, 21);
                 expect(main.model.get('property')).toBe('shape-outside');
             });
 
             it("should match clip-path property", function () {
-                testEditor.setCursorPos(5, 17);
+                constructModelAtPos(5, 17);
                 expect(main.model.get('property')).toBe('clip-path');
             });
 
             it("should match empty circle() value", function () {
-                testEditor.setCursorPos(6, 20);
+                constructModelAtPos(6, 20);
                 expect(main.model.get('value')).toBe('circle()');
             });
 
             it("should match circle() value", function () {
-                testEditor.setCursorPos(7, 20);
-                expect(main.model.get('value')).toBe('circle(0, 0, 0)');
+                constructModelAtPos(7, 20);
+                expect(main.model.get('value')).toBe('circle(0 at 0 0)');
             });
+
             it("should match circle() value when cursor is inside function", function () {
-                testEditor.setCursorPos(7, 27);
-                expect(main.model.get('value')).toBe('circle(0, 0, 0)');
+                constructModelAtPos(7, 27);
+                expect(main.model.get('value')).toBe('circle(0 at 0 0)');
             });
+
             it("should match circle() value with pixel units", function () {
-                testEditor.setCursorPos(8, 27);
-                expect(main.model.get('value')).toBe('circle(0px, 0px, 0px)');
+                constructModelAtPos(8, 27);
+                expect(main.model.get('value')).toBe('circle(0px at 0px 0px)');
             });
+
             it("should match circle() value with mixed units", function () {
-                testEditor.setCursorPos(9, 27);
-                expect(main.model.get('value')).toBe('circle(0px, 0, 0%)');
+                constructModelAtPos(9, 27);
+                expect(main.model.get('value')).toBe('circle(0px at 0 0%)');
             });
+
             it("should match empty ellipse() value", function () {
-                testEditor.setCursorPos(10, 27);
+                constructModelAtPos(10, 27);
                 expect(main.model.get('value')).toBe('ellipse()');
             });
+
             it("should match ellipse() value", function () {
-                testEditor.setCursorPos(11, 27);
-                expect(main.model.get('value')).toBe('ellipse(0, 0, 0, 0)');
+                constructModelAtPos(11, 27);
+                expect(main.model.get('value')).toBe('ellipse(0 0 at 0 0)');
             });
 
             // Incomplete values should not trip the Brackets side.
             // The in-browser CSS Shapes Editor must reject invalid values
             it("should match incomplete ellipse() value", function () {
-                testEditor.setCursorPos(12, 27);
-                expect(main.model.get('value')).toBe('ellipse(0, 0)');
+                constructModelAtPos(12, 27);
+                expect(main.model.get('value')).toBe('ellipse(0 0)');
             });
-            it("should match empty rectangle() value", function () {
-                testEditor.setCursorPos(13, 27);
-                expect(main.model.get('value')).toBe('rectangle()');
-            });
-            it("should match rectangle() value", function () {
-                testEditor.setCursorPos(14, 27);
-                expect(main.model.get('value')).toBe('rectangle(0, 0, 100px, 100px)');
-            });
-            it("should match rectangle() value with border radii", function () {
-                testEditor.setCursorPos(15, 27);
-                expect(main.model.get('value')).toBe('rectangle(0, 0, 100px, 100px, 50%, 50%)');
-            });
-            it("should match rectangle() value with one border radius", function () {
-                testEditor.setCursorPos(16, 27);
-                expect(main.model.get('value')).toBe('rectangle(0, 0, 100px, 100px, 50%)');
-            });
+
             it("should match empty polygon()", function () {
-                testEditor.setCursorPos(17, 27);
+                constructModelAtPos(13, 27);
                 expect(main.model.get('value')).toBe('polygon()');
             });
+
             it("should match polygon() value", function () {
-                testEditor.setCursorPos(18, 27);
+                constructModelAtPos(14, 27);
                 expect(main.model.get('value')).toBe('polygon(0 0, 100px 0, 100px 100px)');
             });
 
             it("should match polygon() value with fill-rule", function () {
-                testEditor.setCursorPos(19, 27);
+                constructModelAtPos(15, 27);
                 expect(main.model.get('value')).toBe('polygon(nonzero, 0 0, 100px 0, 100px 100px)');
             });
         });
 
         describe('Negative match CSS Shapes-like values', function(){
-            var testDocument, testEditor;
 
             beforeEach(function () {
                 var mock = SpecRunnerUtils.createMockEditor(testContentMatchNegative, "css");
@@ -163,29 +165,28 @@ define(function (require, exports, module) {
             });
 
             it("should not match prefixed shape-inside property", function () {
-                testEditor.setCursorPos(1, 27);
+                constructModelAtPos(1, 27);
                 expect(main.model.get('property')).not.toBe('-webkit-shape-inside');
             });
 
             it("should not match commented-out shape-inside property", function () {
-                testEditor.setCursorPos(2, 27);
+                constructModelAtPos(2, 27);
                 expect(main.model.get('property')).not.toBe('shape-inside');
             });
 
             it("should not match non-functional value", function () {
-                testEditor.setCursorPos(3, 27);
+                constructModelAtPos(3, 27);
                 expect(main.model.get('value')).not.toBe('circle');
                 expect(main.model.get('value')).toBe(null);
             });
 
             it("should not match polygon-like value", function () {
-                testEditor.setCursorPos(4, 27);
+                constructModelAtPos(4, 27);
                 expect(main.model.get('value')).not.toBe('fake-polygon()');
             });
         });
 
         describe('Find selector in embedded <style> blocks', function(){
-            var testDocument, testEditor;
 
             beforeEach(function () {
                 var mock = SpecRunnerUtils.createMockEditor(testContentMatchEmbedded, "html");
@@ -200,22 +201,22 @@ define(function (require, exports, module) {
             });
 
             it("should find first selector in head <style>", function () {
-                testEditor.setCursorPos(4, 20);
+                constructModelAtPos(4, 20);
                 expect(main.model.get('selector')).toBe('#content');
             });
 
             it("should find second selector in head <style>", function () {
-                testEditor.setCursorPos(8, 20);
+                constructModelAtPos(8, 20);
                 expect(main.model.get('selector')).toBe('div');
             });
 
             it("should find first selector in body <style>", function () {
-                testEditor.setCursorPos(15, 22);
+                constructModelAtPos(15, 22);
                 expect(main.model.get('selector')).toBe('#content');
             });
 
             it("should find first selector in scoped <style>", function () {
-                testEditor.setCursorPos(21, 22);
+                constructModelAtPos(21, 22);
                 expect(main.model.get('selector')).toBe('#content');
             });
 
